f75fe120659c0c03cb8e613e8f2f2c0f8868a034
Tomash Ghz <ghz.tomash@hotmail.com>
2024-04-10 16:21:22 +0200

    adds main structs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,48 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use netdev::Interface;
+use std::time::SystemTime;
+use std::net::IpAddr;
+
+pub struct NetworkObserver {
+    all_interfaces: bool,
+    public_address: bool,
+    last_state: Option<NetworkState>,
+}
+
+pub struct NetworkState {
+    last_update: SystemTime,
+    default_interface: Interface,
+    all_interfaces: Option<Vec<Interface>>,
+    public_address: Option<IpAddr>,
+}
+
+impl NetworkState {
+    pub fn new() -> Self {
+        Self { last_update: SystemTime::now(), default_interface: netdev::get_default_interface().unwrap(), all_interfaces: None, public_address: None }
+    }
+}
+
+impl NetworkObserver {
+    pub fn new(all_interfaces: bool, public_address: bool) -> Self {
+        let current_state = NetworkState::new();
+        NetworkObserver {
+            all_interfaces,
+            public_address,
+            last_state: None,
+        }
+    }
+    
+    pub fn state_changed(&mut self) -> bool {
+        let current_state = NetworkState::new();
+        //update state 
+        self.last_state = Some(current_state);
+        true
+    }
+}
+
+impl Drop for NetworkObserver {
+    fn drop(&mut self) {
+        
+    }
 }
 
 #[cfg(test)]
@@ -8,7 +51,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let observer = NetworkObserver::new(false, false);
+        assert!(observer.state_changed());
     }
 }
