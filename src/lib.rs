3927c656b3d5fbfcc7fc43c322c2288f523d7c54
Tomash Ghz <ghz.tomash@hotmail.com>
2024-04-10 17:29:37 +0200

    compare default_interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
 use netdev::Interface;
-use std::time::SystemTime;
+use public_ip_address::lookup::LookupProvider;
 use std::net::IpAddr;
+use std::time::SystemTime;
 
 pub struct NetworkObserver {
     all_interfaces: bool,
     public_address: bool,
-    last_state: Option<NetworkState>,
+    last_state: NetworkState,
 }
 
+#[derive(Debug)]
 pub struct NetworkState {
     last_update: SystemTime,
     default_interface: Interface,
@@ -15,34 +17,74 @@ pub struct NetworkState {
     public_address: Option<IpAddr>,
 }
 
+impl PartialEq for NetworkState {
+    fn eq(&self, other: &Self) -> bool {
+        self.default_interface == other.default_interface
+    }
+}
+
 impl NetworkState {
     pub fn new() -> Self {
-        Self { last_update: SystemTime::now(), default_interface: netdev::get_default_interface().unwrap(), all_interfaces: None, public_address: None }
+        Self {
+            last_update: SystemTime::now(),
+            default_interface: netdev::get_default_interface().unwrap(),
+            all_interfaces: None,
+            public_address: None,
+        }
     }
 }
 
 impl NetworkObserver {
     pub fn new(all_interfaces: bool, public_address: bool) -> Self {
-        let current_state = NetworkState::new();
+        let mut current_state = NetworkState::new();
+        if all_interfaces {
+            current_state.all_interfaces = Some(netdev::get_interfaces());
+        }
+        if public_address {
+            if let Ok(response) = public_ip_address::perform_cached_lookup_with(
+                vec![
+                    (LookupProvider::MyIpCom, None),
+                    (LookupProvider::GetJsonIp, None),
+                    (LookupProvider::Ipify, None),
+                    (LookupProvider::IpInfo, None),
+                ],
+                None,
+                Some(5),
+                false,
+            ) {
+                current_state.public_address = Some(response.ip);
+            }
+        }
         NetworkObserver {
             all_interfaces,
             public_address,
-            last_state: None,
+            last_state: current_state,
         }
     }
-    
+
     pub fn state_changed(&mut self) -> bool {
         let current_state = NetworkState::new();
-        //update state 
-        self.last_state = Some(current_state);
-        true
+        let mut state_changed = false;
+
+        if self.last_state.default_interface != current_state.default_interface {
+            state_changed = true;
+        }
+        if self.all_interfaces {
+
+        }
+        if self.public_address {
+
+        }
+        if state_changed {
+            //update state
+            self.last_state = current_state;
+        }
+        state_changed
     }
 }
 
 impl Drop for NetworkObserver {
-    fn drop(&mut self) {
-        
-    }
+    fn drop(&mut self) {}
 }
 
 #[cfg(test)]
@@ -51,7 +93,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let observer = NetworkObserver::new(false, false);
+        let mut observer = NetworkObserver::new(false, false);
         assert!(observer.state_changed());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use network_changed::NetworkObserver;
+use std::io;
+
 fn main() {
-    println!("hello world");
+    let mut input = String::new();
+    let mut observer = NetworkObserver::new(false, false);
+    loop {
+        println!("Network status changed: {}", observer.state_changed());
+        _ = io::stdin().read_line(&mut input);
+    }
 }
