850dcfa39c81467ef95aad9aa96ce02058b8b2f1
Tomash Ghz <ghz.tomash@hotmail.com>
2024-04-10 22:17:26 +0200

    expire first state

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,44 @@
 use netdev::Interface;
 use public_ip_address::lookup::LookupProvider;
 use std::net::IpAddr;
+use std::time::Duration;
 use std::time::SystemTime;
 
+static DEFAULT_EXPIRE_TIME: u64 = 3600;
+
 pub struct NetworkObserver {
-    all_interfaces: bool,
-    public_address: bool,
+    config: ObserverConfig,
     last_state: NetworkState,
+}
+
+pub struct ObserverConfig {
+    pub expire_time: u64,
+    pub all_interfaces: bool,
+    pub public_address: bool,
+}
+
+impl ObserverConfig {
+    pub fn new(expire_time: u64, all_interfaces: bool, public_address: bool) -> Self {
+        Self {
+            expire_time,
+            all_interfaces,
+            public_address,
+        }
+    }
+
+    pub fn default() -> Self {
+        Self {
+            expire_time: 3600,
+            all_interfaces: false,
+            public_address: false,
+        }
+    }
 }
 
 #[derive(Debug)]
 pub struct NetworkState {
     last_update: SystemTime,
-    default_interface: Interface,
+    default_interface: Option<Interface>,
     all_interfaces: Option<Vec<Interface>>,
     public_address: Option<IpAddr>,
 }
@@ -27,20 +53,66 @@ impl NetworkState {
     pub fn new() -> Self {
         Self {
             last_update: SystemTime::now(),
-            default_interface: netdev::get_default_interface().unwrap(),
+            default_interface: netdev::get_default_interface().ok(),
             all_interfaces: None,
             public_address: None,
         }
+    }
+
+    fn compare(&self, other: &Self, config: &ObserverConfig) -> bool {
+        // check expire time
+        if other
+            .last_update
+            .duration_since(self.last_update)
+            .unwrap_or_default()
+            .as_secs()
+            >= config.expire_time
+        {
+            println!("State expired");
+            return true;
+        }
+
+        // check default interface
+        if self.default_interface != other.default_interface {
+            println!("Default interface changed");
+            return true;
+        }
+        if config.all_interfaces {
+            if self.all_interfaces != other.all_interfaces {
+                println!("Non defalt interfaces changed");
+                return true;
+            }
+        }
+        if config.public_address {
+            if self.public_address != other.public_address {
+                println!("Public address changed");
+                return true;
+            }
+        }
+
+        return false;
     }
 }
 
 impl NetworkObserver {
     pub fn new(all_interfaces: bool, public_address: bool) -> Self {
         let mut current_state = NetworkState::new();
-        if all_interfaces {
+        // expire the state
+        current_state.last_update -= Duration::from_secs(DEFAULT_EXPIRE_TIME);
+
+        let config = ObserverConfig::new(DEFAULT_EXPIRE_TIME, all_interfaces, public_address);
+        NetworkObserver {
+            config,
+            last_state: current_state,
+        }
+    }
+
+    pub fn current_state(&self) -> NetworkState {
+        let mut current_state = NetworkState::new();
+        if self.config.all_interfaces {
             current_state.all_interfaces = Some(netdev::get_interfaces());
         }
-        if public_address {
+        if self.config.public_address {
             if let Ok(response) = public_ip_address::perform_cached_lookup_with(
                 vec![
                     (LookupProvider::MyIpCom, None),
@@ -55,26 +127,13 @@ impl NetworkObserver {
                 current_state.public_address = Some(response.ip);
             }
         }
-        NetworkObserver {
-            all_interfaces,
-            public_address,
-            last_state: current_state,
-        }
+        current_state
     }
 
     pub fn state_changed(&mut self) -> bool {
-        let current_state = NetworkState::new();
-        let mut state_changed = false;
+        let current_state = self.current_state();
+        let state_changed = self.last_state.compare(&current_state, &self.config);
 
-        if self.last_state.default_interface != current_state.default_interface {
-            state_changed = true;
-        }
-        if self.all_interfaces {
-
-        }
-        if self.public_address {
-
-        }
         if state_changed {
             //update state
             self.last_state = current_state;
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,24 @@ use std::io;
 
 fn main() {
     let mut input = String::new();
-    let mut observer = NetworkObserver::new(false, false);
+    let mut observer = NetworkObserver::new(true, false);
     loop {
         println!("Network status changed: {}", observer.state_changed());
-        _ = io::stdin().read_line(&mut input);
+        match io::stdin().read_line(&mut input) {
+            Ok(r) => {
+                if r == 0 {
+                    break;
+                }
+                match input.trim() {
+                    "quit" | "q" => break,
+                    _ => (),
+                }
+                input.clear();
+            }
+            Err(error) => {
+                println!("error: {}", error);
+                break;
+            }
+        }
     }
 }
